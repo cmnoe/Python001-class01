e87c91b06c6686e8880a3760c542e6ed22dfea6a
cmnoe <18307985226@126.com>
2020-08-23 23:58:00 +0800

    week09

---
+++ week09/Django-login/MyDjango/login/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .form import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+# Create your views here.
+def login2(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user: 
+                login(request, user)
+                return HttpResponse('登录成功')
+            else: 
+                return render(request, 'return.html')
+    if request.method == 'GET':
+        login_form = LoginForm()
+        return render(request, 'form.html', { 'form': login_form })
+
